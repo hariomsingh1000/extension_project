535fca44daf7892924052536a4d902be92b2af56
Hariom kumar singh <hariomsingh.gec@gmail.com>
2021-03-05 00:46:03 +0530

    it is simple hello world file

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+   public static void main(String[] args){
+
+           for(int i=1;i<=10;i++){
+        system.out.println("Hello Extension technology .."+1);
+          }
+      }
+  }
