19a01c596a0e25084be5477e125a6895ab195f18
Hariom kumar singh <hariomsingh.gec@gmail.com>
2021-03-05 01:10:56 +0530

    it is a java program

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld{
-   public static void main(String[] args){
+    public static void main(String[] args){
 
            for(int i=1;i<=10;i++){
         system.out.println("Hello Extension technology .."+1);
