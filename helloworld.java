e949a8c970de236e63f4a041adc652347206484e
Hariom kumar singh <hariomsingh.gec@gmail.com>
2021-03-05 11:49:28 +0530

    this is java program

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld{
     public static void main(String[] args){
 
-           for(int i=1;i<=10;i++){
+          for(int i=1;i<=10;i++){
         system.out.println("Hello Extension technology .."+1);
           }
       }
